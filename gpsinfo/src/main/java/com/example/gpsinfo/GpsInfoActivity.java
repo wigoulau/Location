a88424df224da6488fcca68565a860910a618018
wigoulau <liuwg2005@126.com>
2021-01-08 17:00:43 +0800

    new gps project

---
+++ gpsinfo/src/main/java/com/example/gpsinfo/GpsInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.gpsinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GpsInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_info);
+    }
+}
