9a4dfcce9dc412e4d9182142f38bca602a22cbca
wigoulau <liuwg2005@126.com>
2021-01-08 16:59:01 +0800

    new project

---
+++ modeminfo/src/main/java/com/example/modeminfo/ModemInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.modeminfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModemInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modem_info);
+    }
+}
